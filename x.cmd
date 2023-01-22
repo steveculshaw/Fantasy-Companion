f97c383aa852b46ec314231e0159728922d0657d
Steve Culshaw <steveculshaw@gmail.com>
2023-01-22 20:00:52 +0000

    build 0.0.8

---
+++ x.cmd
@@ -0,0 +1,53 @@
+@echo off
+
+cls
+
+set version=0.0.8
+
+REM =================================================
+REM Run Script Fantasy Compendium BUILD {rsfcbuild}
+REM =================================================
+
+echo.
+echo building Fantasy Companion version: %VERSION%
+
+:START
+pushd
+cd F:\Fantasy-Companion\
+f:
+
+set tgt=Fantasy-Companion.zip
+
+REM echo.
+REM echo ================================================================
+REM echo   DID I UPDATE THE VERSION IN MODULE.JSON !!!!
+REM echo.
+REM echo   also update the readme.md with version for ease of checking !
+REM echo ================================================================
+
+echo.
+echo remove old zip ...
+if exist %tgt% del %tgt%
+echo create new zip ...
+7z a -r -tzip -aoa -xr!.git -x!*.py -x!*.sh %tgt% *.*
+
+echo.
+echo.
+echo git commit changes for version: %VERSION% ...
+REM git commit -a -m "stage and commit from Fantasy-Companion build script version:%VERSION%"
+
+echo.
+echo.
+echo push changes to GitHub ...
+REM git push origin master
+
+:ALLDONE
+REM popd
+REM c:
+
+echo.
+echo all done ...
+
+REM pause
+
+REM timeout 5
